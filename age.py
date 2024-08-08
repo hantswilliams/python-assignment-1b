0ce4f13a05b3fdf66f503f4b50e15899cf506443
hantswilliams <hantsawilliams@gmail.com>
2024-08-08 23:24:03 +0000

    added a new python age.py file

---
+++ age.py
@@ -0,0 +1,9 @@
+inputted_value = input('Give me your age and I will multiply it by 2: ')
+
+print(type(inputted_value))
+
+inputted_value_number = int(inputted_value)
+
+output = inputted_value_number * 2
+
+print(f'Your age doubled is {output}')
